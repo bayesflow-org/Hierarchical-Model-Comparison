ba1856d37329d0ef2a5d1e2507c4ab4b051b9f0a
elseml <60779710+elseml@users.noreply.github.com>
2023-06-13 14:20:00 +0200

    adapt to new BayesFlow version

--- notebooks/02_bridge_sampling_comparison/non_nested_models/hyperparameter_tuning.py
+++ notebooks/02_bridge_sampling_comparison/non_nested_models/hyperparameter_tuning.py
@@ -4,100 +4,111 @@ import os, sys
 #sys.path.append(os.path.abspath(os.path.join('../../..'))) # access sibling directories; as in .ipynb
 
 # When on desktop (requires different paths than cloud)
-sys.path.append(os.path.abspath(os.path.join('../Coding'))) # access sibling directories; different than .ipynb
-sys.path.append("C:\\Users\\lasse\\Documents\\GitHub\\BayesFlow")
+sys.path.append(os.path.abspath(os.path.join('../../..'))) # access sibling directories; different than .ipynb
+sys.path.append(os.path.abspath(os.path.join('../../../../BayesFlow_dev/BayesFlow/')))
 
-from src.python.settings import summary_meta_validation, evidence_meta_validation
-from src.python.networks import HierarchicalInvariantNetwork, EvidentialNetwork
-from src.python.models import HierarchicalSdtMptSimulator, MainSimulator
-from src.python.losses import softmax_loss
-from src.python.helpers import n_clust_obs_f_f, get_multiple_predictions, get_preds_and_bfs
+from src.python.models import HierarchicalSdtMptSimulator
 
 import numpy as np
+import bayesflow as bf
 import pandas as pd
-from tensorflow.keras.experimental import CosineDecay
-from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
+from time import perf_counter
 
 from functools import partial
 
-from bayesflow.trainers import ModelComparisonTrainer
-from bayesflow.amortizers import MultiModelAmortizer
 
 # For testing: n_runs_per_setting = 1, epochs=2, iterations_per_epoch=2
 # For running: n_runs_per_setting = 5, epochs=15, iterations_per_epoch=1000
 
 #### Runs per setting combination 
-n_runs_per_setting = 5
+n_runs_per_setting = 2#5
 
 ### Hyperparameter tuning params (cosine decay w/ restarts)
-dropout_list = [False, True]
-input_noise_list = [None, 0.05]
+bigger_network_128_units = [False, True]
+smaller_lr_5e_5 = [False, True]
 
-n_combinations = len(dropout_list)*len(input_noise_list)*n_runs_per_setting
+n_combinations = len(bigger_network_128_units)*len(smaller_lr_5e_5)*n_runs_per_setting
 
 ### prepare static components
 results = []
-
-# Training steps
-epochs=15 
-iterations_per_epoch=1000
-
-# Cosine decaying learning rate
-initial_lr = 0.0005
-decay_steps = epochs*iterations_per_epoch
-alpha = 0
-lr_schedule = CosineDecay(
-    initial_lr, decay_steps, alpha=alpha
-    )
 
 # Sample size
 n_clusters = 25
 n_obs = 50
 
+# Generative models
+sdtmpt_model = HierarchicalSdtMptSimulator()
+
+sdt_simulator = partial(sdtmpt_model.generate_batch, model_index=0, n_clusters=n_clusters, n_obs=n_obs, n_vars=2)
+mpt_simulator = partial(sdtmpt_model.generate_batch, model_index=1, n_clusters=n_clusters, n_obs=n_obs, n_vars=2)
+meta_model = bf.simulation.MultiGenerativeModel([sdt_simulator, mpt_simulator])
+
+# Training steps
+epochs=3#20
+iterations_per_epoch=10#00
+
 # Storage path
-file_path = os.path.join(os.getcwd(),'notebooks\\02_bridge_sampling_comparison\\non_nested_models\\hyperparameter_tuning_results_dropout_input_noise')
+file_path = os.path.join(os.getcwd(),'hyperparameter_tuning_results_size_lr')
 
 ### Run tuning
-for dropout in dropout_list:
-    for input_noise in input_noise_list:
+for bigger_size in bigger_network_128_units:
+    for smaller_lr in smaller_lr_5e_5:
         for run in range(n_runs_per_setting):
 
-            if dropout == True:
-                evidence_meta_validation.update({'dropout': True})
-            if dropout == False:
-                evidence_meta_validation.update({'dropout': False})
+            if bigger_network_128_units == True:
+                config = {"units": 128, "activation": "relu", "kernel_initializer": "glorot_uniform"}
+                summary_net = bf.summary_networks.HierarchicalNetwork([
+                    bf.networks.DeepSet(dense_s1_args=config, dense_s2_args=config, dense_s3_args=config), 
+                    bf.networks.DeepSet(dense_s1_args=config, dense_s2_args=config, dense_s3_args=config)
+                    ])
+            else:
+                summary_net = bf.summary_networks.HierarchicalNetwork([
+                    bf.networks.DeepSet(), 
+                    bf.networks.DeepSet()
+                    ])
     
             # Initialize from scratch for each run
-            summary_net = HierarchicalInvariantNetwork(summary_meta_validation)
-            evidence_net = EvidentialNetwork(evidence_meta_validation)
-    
-            amortizer = MultiModelAmortizer(evidence_net, summary_net)
-    
-            simulator = MainSimulator(HierarchicalSdtMptSimulator())
-    
-            trainer = ModelComparisonTrainer(
-                network=amortizer, 
-                generative_model=simulator, 
-                loss=partial(softmax_loss),
-                optimizer=partial(Adam, lr_schedule),
-                skip_checks=True,
+            probability_net = bf.inference_networks.PMPNetwork(num_models=2, dropout=False)
+            amortizer = bf.amortizers.AmortizedModelComparison(probability_net, summary_net)
+
+            if smaller_lr:
+                initial_lr = 0.00005 # smaller than default!
+                schedule = tf.keras.optimizers.schedules.CosineDecay(
+                    initial_lr, iterations_per_epoch * epochs, name="lr_decay"
                 )
-    
+                optimizer = tf.keras.optimizers.Adam(schedule)
+                trainer = bf.trainers.Trainer(
+                    amortizer=amortizer, 
+                    generative_model=meta_model,
+                    optimizer=optimizer
+                    )
+                
+            else:
+                trainer = bf.trainers.Trainer(
+                    amortizer=amortizer, 
+                    generative_model=meta_model
+                    )
+        
             # Train
+            training_time_start = perf_counter()
             losses = trainer.train_online(
-                epochs=1, iterations_per_epoch=1, batch_size=32, 
-                n_obs=partial(n_clust_obs_f_f, n_clusters, n_obs), n_vars=2,
-                add_noise_std=input_noise
+                epochs=epochs, 
+                iterations_per_epoch=iterations_per_epoch, 
+                batch_size=32
                 )
+            training_time_stop = perf_counter()
     
-            # Get running loss of final epoch
-            final_loss = np.mean(list(losses.values())[-1])
-    
+            # Get running loss of final epoch and training time
+            final_loss = np.mean(losses[-iterations_per_epoch:])
+            training_time = training_time_stop-training_time_start
+
             # Store results
             results.append({
-                'dropout': dropout,
-                'input noise': input_noise,
-                'final_loss': final_loss
+                'bigger_size': bigger_size,
+                'smaller_lr': smaller_lr,
+                'final_loss': final_loss,
+                'training_time': training_time
             })
     
             # Print progress & secure progess
