de7495c1b0a26b8e07ecee6e79322bca18a0f50c
elseml <60779710+elseml@users.noreply.github.com>
2023-07-06 10:24:50 +0200

    streamline validation settings

--- src/python/settings.py
+++ src/python/settings.py
@@ -1,62 +1,32 @@
 # Network settings for validation experiment 1
-summary_meta_validation = {
+summary_meta_validation_1 = {
     "level_1": {
-        "inv_inner": {
-            "dense_inv_pre_pooling_args": dict(
+        "dense_s1_args": dict(
                 units=8, activation="elu", kernel_initializer="glorot_normal"
             ),
-            "dense_inv_post_pooling_args": dict(
-                units=8, activation="elu", kernel_initializer="glorot_normal"
-            ),
-            "n_dense_inv": 2,
-        },
-        "inv_outer": {
-            "dense_inv_pre_pooling_args": dict(
+        "dense_s2_args": dict(
                 units=32, activation="elu", kernel_initializer="glorot_normal"
             ),
-            "dense_inv_post_pooling_args": dict(
-                units=32, activation="elu", kernel_initializer="glorot_normal"
-            ),
-            "n_dense_inv": 2,
-        },
-        "dense_equiv_args": dict(
-            units=16, activation="elu", kernel_initializer="glorot_normal"
-        ),
-        "n_dense_equiv": 2,
-        "n_equiv": 2,
-    },
+        "dense_s3_args": dict(
+                units=16, activation="elu", kernel_initializer="glorot_normal"
+            )
+    }, 
     "level_2": {
-        "inv_inner": {
-            "dense_inv_pre_pooling_args": dict(
+        "dense_s1_args": dict(
                 units=32, activation="elu", kernel_initializer="glorot_normal"
             ),
-            "dense_inv_post_pooling_args": dict(
-                units=32, activation="elu", kernel_initializer="glorot_normal"
-            ),
-            "n_dense_inv": 2,
-        },
-        "inv_outer": {
-            "dense_inv_pre_pooling_args": dict(
+        "dense_s2_args": dict(
                 units=128, activation="elu", kernel_initializer="glorot_normal"
             ),
-            "dense_inv_post_pooling_args": dict(
-                units=128, activation="elu", kernel_initializer="glorot_normal"
-            ),
-            "n_dense_inv": 2,
-        },
-        "dense_equiv_args": dict(
-            units=64, activation="elu", kernel_initializer="glorot_normal"
-        ),
-        "n_dense_equiv": 2,
-        "n_equiv": 2,
-    },
+        "dense_s3_args": dict(
+                units=64, activation="elu", kernel_initializer="glorot_normal"
+            )
+    }
 }
-probability_meta_validation = {
-    "dense_args": dict(units=64, activation="elu", kernel_initializer="glorot_normal"),
-    "n_dense": 2,
-    "n_models": 2,
+        
+probability_meta_validation_1 = {
+    "dense_args": dict(units=64, activation="elu", kernel_initializer="glorot_normal")
 }
-
 
 # Network settings for validation experiment 2
 summary_meta_validation_2 = {
@@ -87,7 +57,6 @@ summary_meta_validation_2 = {
 probability_meta_validation_2 = {
     "dense_args": dict(units=128, activation="elu", kernel_initializer="glorot_normal")
 }
-
 
 # Network settings for levy flight application
 summary_meta_diffusion = {
